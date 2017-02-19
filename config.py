d2b97efe11f9dca51cd21bd5a68ea45bfb2196b0
Craig Harris <cshseattle@gmail.com>
2017-02-18 22:30:37 -0800

    push 5

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'cshTest123'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
